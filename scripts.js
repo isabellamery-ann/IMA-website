85089310a847c481df7610517c9606f406642b90
isabellamery-ann <117369679+isabellamery-ann@users.noreply.github.com>
2023-01-16 10:33:06 +0100

    slick-slider

--- scripts.js
+++ scripts.js
@@ -28,3 +28,19 @@
       speed: 500,
       download: false
     });
+
+// Slick Slider 
+
+    $('.testimonial-slider-container').slick({
+      autoplay: true,        // Do we want it to autoplay? true or false
+      autoplaySpeed: 4000,   // How long between each slide when autoplaying
+      speed: 500,            // How fast is the transition 
+      arrows: true,          // Do you want to show arrows to trigger each slide
+      accessibility: true,   // Enables tabbing and arrow key navigation 
+      dots: true,            // Enables the dots below to show how many slides
+      fade: false,           // Changes the animate from slide to fade if true
+      infinite: true,       // When true, means that it will scroll in a circle
+      pauseOnHover: true,   // When true means the autoplay pauses when hovering
+      pauseOnDotsHover: true // Pauses the autoplay when hovering over the dots
+    });
+    
